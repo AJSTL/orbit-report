f0e67779e4210bb699885b548e97c4598270d617
AJ Meyer <ashley.meyer@gmail.com>
2019-12-18 18:59:36 -0600

    part 6 completed

--- src/app/satellite.ts
+++ src/app/satellite.ts
@@ -12,6 +12,15 @@ export class Satellite {
         this.orbitType = orbitType;
         this.operational = operational;
     }
+
+    shouldShowWarning() {
+        if (this.type.toUpperCase() == "SPACE DEBRIS") {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
 }
 
  
